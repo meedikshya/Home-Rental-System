e3903e0965915dbea26cd1fcc2ad0f6f13c05123
Dikshya <dikxu7611@gmail.com>
2024-06-29 20:08:39 +0545

    updated-files-folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/random")
+      .then((response) => {
+        setJokes(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Jokes</h1>
+      <p>JOKES: {jokes.length} </p>
+      {jokes.map((joke) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
