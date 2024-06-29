4ed176661a9f5268753e56ab6d9fea41375ec854
Dikshya Sharma <dikxu22@gmail.com>
2024-06-29 20:08:39 +0545

    updated-files-folders

--- server/src/index.js
+++ server/src/index.js
@@ -11,6 +11,22 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/api/random", (req, res) => {
+  const random = [
+    {
+      id: 1,
+      title: "Random",
+      content: "bjskskdhwdio",
+    },
+    {
+      id: 2,
+      title: "Random",
+      content: "bjskskdhwdio",
+    },
+  ];
+  res.json(random);
+});
+
 app.get("/login", (req, res) => {
   res.send("<h2> Login </h2>");
 });
